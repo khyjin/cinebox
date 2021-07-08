e851048e04fadc11926fc16038aad56bc1377337
sohyun <jsnn5279@naver.com>
2021-07-08 16:52:51 +0900

    create ticket part

---
+++ src/main/java/com/bookshop01/ticket/dao/TicketDAO.java
@@ -0,0 +1,13 @@
+package com.bookshop01.ticket.dao;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+import com.bookshop01.goods.vo.GoodsVO;
+
+public interface TicketDAO {
+
+	public List<GoodsVO> listGoods() throws DataAccessException;
+
+}
